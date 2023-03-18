00e576d1a381407074dcbab4feb79011ba6f2a86
seadams <seadams1400@gmail.com>
2023-03-18 09:39:12 +0100

    switched build tooling to vite

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import path from 'node:path';
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@firebase/db': path.resolve(__dirname, './src/firebase/db'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    }
+  }
+})
